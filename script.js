118c06bb74b1483408e6a88c35e7a95defaf99a4
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-18 18:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,9 @@ function promiseOne()
 	return new Promise((resolve,reject)=>{
 		var num1 = Math.floor(Math.random() * 3) + 1;
 		setTimeout(()=>{
-			
-			var row = tables.insertRow();
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
 			cell1.innerHTML= "Promise 1";
 			cell2.innerHTML = num1;
+			resolve(num1);
 		},num1);
 	})
 }
@@ -33,6 +30,7 @@ function promiseSecond()
             var cell2 = row.insertCell(1);
 			cell1.innerHTML= "Promise 2";
 			cell2.innerHTML = num1;
+			resolve(num1);
 		},num1);
 	})
 }
@@ -48,28 +46,24 @@ function promiseThree()
             var cell2 = row.insertCell(1);
 			cell1.innerHTML= "Promise 3";
 			cell2.innerHTML = num1;
+			resolve(num1);
 		},num1);
 	})
 }
 
-function promiseFourth()
-{
-	return new Promise((resolve,reject)=>{
-		var num1 = Math.floor(Math.random() * 3) + 1;
-		setTimeout(()=>{
-			var row = tables.insertRow();
+
+Promise.all([promiseOne(),promiseSecond(),promiseThree()])
+.then((results)=>{
+	var sum =0;
+	for(let i=0;i<results.length;i++)
+		{
+			sum = sum + results[i];
+		}
+	var row = tables.insertRow();
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
 			cell1.innerHTML= "Total";
-			cell2.innerHTML = num1;
-		},num1);
-	})
-}
-
-
-Promise.all([promiseOne(),promiseSecond(),promiseThree(),promiseFourth()])
-.then((results)=>{
-	console.log("completed");
+			cell2.innerHTML = sum;
 })
 .catch((error))
 {
